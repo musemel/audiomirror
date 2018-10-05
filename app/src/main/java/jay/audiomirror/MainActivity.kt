0297e8d9cd5acbb70760d9b7a61acdf6ae036177
Jay <jaylineko@gmail.com>
2018-10-05 23:21:47 +0200

    Request mic permissions and service starting logic

--- app/src/main/java/jay/audiomirror/MainActivity.kt
+++ app/src/main/java/jay/audiomirror/MainActivity.kt
@@ -1,5 +1,10 @@
 package jay.audiomirror
 
+import android.Manifest.permission.RECORD_AUDIO
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,5 +13,29 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
+
+    runService()
+  }
+
+  private fun runService() {
+    if (SDK_INT >= 23 && checkSelfPermission(RECORD_AUDIO) != PERMISSION_GRANTED) {
+      requestPermissions(arrayOf(RECORD_AUDIO), 0)
+      return
+    }
+
+    startFgService(Intent(this, AudioMirrorService::class.java))
+  }
+
+  private fun startFgService(intent: Intent): ComponentName? =
+    if (SDK_INT >= 28) startForegroundService(intent) else startService(intent)
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray
+  ) {
+    if (grantResults.getOrNull(permissions.indexOf(RECORD_AUDIO)) == PERMISSION_GRANTED) {
+      runService()
+    }
   }
 }
